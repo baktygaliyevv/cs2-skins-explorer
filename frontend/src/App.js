498a008119bdc49491928f199545f03ae2113728
Nuraly <kracken787@gmail.com>
2024-01-22 17:34:37 +0000

    initialized django and react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+    <h1>New journey begins</h1>
+    <Button variant="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
