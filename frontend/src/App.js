46adae493b76128a8b18e190fa27a4ca441a3ba6
Nuraly <kracken787@gmail.com>
2024-01-24 18:43:49 +0000

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 function App() {
   return (
-    <>
-    <h1>New journey begins</h1>
-    <Button variant="primary">Button</Button>
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
 
